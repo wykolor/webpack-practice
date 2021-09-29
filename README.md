6197f5dde57d40ae1a94983f69866828c98d4f79
wykolor <kolor1121460147@163.com>
2021-09-29 17:27:34 +0800

    feat: 自动清除构建目录产物

---
+++ README.md
@@ -0,0 +1,62 @@
+
+## Loader
+
+**file-loader & url-loader**
+- 都可以用于处理文件 比如字体文件，图片
+- url-loader可以设置较小资源自动 base64
+
+## Plugin
+
+
+## webpack热更新及原理分析
+
+**webpack-dev-server** 
+
+- WDS 不刷新浏览器
+- WDS 不输出文件（没有磁盘的I/O），而是放在内存中
+- 使用HotModuleReplacementPlugin插件
+
+**webpack-dev-middleware**
+
+- WDM将webpack输出的文件传输给服务器
+-  适应于灵活的配置场景
+
+
+**原理分析**
+
+webpack Compile：将JS编译成Bundle
+
+HMR Server： 将热更新的文件传输给 HMR Runtime
+
+Bundle server：提供文件在浏览器的访问
+
+HMR Runtime: 会被注入到浏览器，更新文件的变化
+
+bundle.js：构建输出的文件
+
+![avatar](./src/assets/images/hot-dev-ser.png)
+
+
+## 文件指纹 
+
+- 打包后出现的文件名的后缀
+
+**chunkhash**：和webpack打包的chunk有关，不同的entry会生成的不同的chunkhash值
+
+**contenthash**：根据文件内容来定义hash，文件内容不变，则contenthash不变
+
+**hash**：和整个项目的构建相关，只要项目文件有修改，整个项目构建的hash值就会更改
+
+
+## 文件压缩
+
+**js压缩**: 内置了uglifyjs-webpack-plugin，js文件默认是压缩的
+
+**css压缩**：optimize-css-assets-webpack-plugin，同时使用cssnano
+
+**html文件的压缩**： html-webpack-plugin,设置压缩参数
+
+## 自动清理构建目录产物
+
+**clean-webpack-plugin**
+- 默认会删除output指定的输出目录
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 
 const path = require('path');
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
 module.exports = {
   entry:{
@@ -24,7 +26,6 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader', // loader的调用是链式调用的，顺序是从右到左
-          'less-loader'
         ]
       },
       {
@@ -52,14 +53,32 @@ module.exports = {
       }
     ]
   },
-  watch: true, // 监听通过轮训
+  // watch: true, // 监听通过轮训
   watchOptions: {
     ignored: /node_modules/, // 忽略需要监听的文件
     aggregateTimeout: 1000, // 在该配置时间内的更改都汇聚成一次变更
     poll: 1000
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    // 压缩HTML
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: false
+      }
+
+    }),
+    // 清除构建目录的文件
+    new CleanWebpackPlugin()
   ],
   devServer: {
     contentBase: './dist',
